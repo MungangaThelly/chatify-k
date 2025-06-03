5f327458f5089dd4ba850a552bfa6ebe0ba15395
MungangaThelly <daddymulongo@yahoo.fr>
2025-06-03 21:07:32 +0200

    update disabledsyntrydnsforbilling

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import * as Sentry from '@sentry/react';
+//import * as Sentry from '@sentry/react';
 //import './index.css';
 import { AuthProvider } from './context/AuthContext'; // om du använder context
 
-Sentry.init({
+/*Sentry.init({
   dsn: import.meta.env.VITE_SENTRY_DSN,
   sendDefaultPii: true,
   environment: import.meta.env.MODE,
-});
+});*/
 
 
 const container = document.getElementById('app');
